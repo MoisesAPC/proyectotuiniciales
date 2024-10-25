8900cc74dc2fbac5f43e41cb21a375d864e84d9c
MoisesAPC <moipc647@gmail.com>
2024-10-25 09:50:53 +0100

    pagina de error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import Login from './pages/Login'
 import Home from './pages/Home'
 import Reports from './pages/Reports'
+import ErrorPage from './pages/ErrorPage'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
     path: '/',
+    errorElement: <ErrorPage/>,
     children: [
       {
         index: true,
