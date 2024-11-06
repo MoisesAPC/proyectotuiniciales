5d3068a2921235c129ff1814256cc9efcfa10185
MoisesAPC <moipc647@gmail.com>
2024-11-06 20:34:53 +0000

    Añadidos mas botones en el Drawer

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -12,6 +12,10 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import HomeIcon from '@mui/icons-material/Home';
+import ArticleIcon from '@mui/icons-material/Article';
+import HelpIcon from '@mui/icons-material/Help';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import Drawer from '@mui/material/Drawer';
 
 function Menu() {
   const [open, setOpen] = React.useState(false);
@@ -25,12 +29,56 @@ function Menu() {
     <Box sx={{ maxwidth: 250 }} role="presentation" onClick={toggleDrawer(false)}>
       <List>
         <Link to={'/home'} style={{textDecoration:'none', color:'black'}}>
+          {/* Inicio */}
           <ListItem disablePadding>
             <ListItemButton>
+
               <ListItemIcon>
                 <HomeIcon />
               </ListItemIcon>
-              <ListItemText primary="Inicio" />
+
+              {/* Ponemos el texto de inicio en negrita y en blanco (obtenido del color primario de nuestro tema personalizado) */}
+              <ListItemText primary="Inicio" sx={{ fontWeight: 'bold', color: 'primary.dark' }}/>
+
+            </ListItemButton>
+          </ListItem>
+
+          {/* Informes */}
+          <ListItem disablePadding>
+            <ListItemButton>
+
+              <ListItemIcon>
+                <ArticleIcon />
+              </ListItemIcon>
+              
+              <ListItemText primary="Informes" sx={{ fontWeight: 'bold', color: 'primary.dark' }}/>
+
+            </ListItemButton>
+          </ListItem>
+
+          {/* Ayuda */}
+          <ListItem disablePadding>
+            <ListItemButton>
+
+              <ListItemIcon>
+                <HelpIcon />
+              </ListItemIcon>
+              
+              <ListItemText primary="Ayuda" sx={{ fontWeight: 'bold', color: 'primary.dark' }}/>
+
+            </ListItemButton>
+          </ListItem>
+
+          {/* Salir */}
+          <ListItem disablePadding>
+            <ListItemButton>
+
+              <ListItemIcon>
+                <ExitToAppIcon />
+              </ListItemIcon>
+              
+              <ListItemText primary="Salir" sx={{ fontWeight: 'bold', color: 'primary.dark' }}/>
+
             </ListItemButton>
           </ListItem>
         </Link>
@@ -41,14 +89,24 @@ function Menu() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
+
         <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" onClick={toggleDrawer(true)} aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
+          <IconButton edge="start" color="primary" onClick={toggleDrawer(true)} aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon/>
           </IconButton>
           <Typography variant="h6" color="inherit" component="div">
             Photos
           </Typography>
         </Toolbar>
+
+        <Drawer
+          anchor="left"
+          open={open}
+          onClose={toggleDrawer(false)}
+        >
+          {DrawerList}
+        </Drawer>
+
       </AppBar>
     </Box>
   );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,58 +1,18 @@
-import { Typography, Button } from '@mui/material';
-import { useNavigate } from 'react-router-dom'
-
 // Importamos el useSelector del react-redux
 import { useSelector } from 'react-redux'
 // Importamos lo que necesitamos para el tipo del selector()
 import { RootState} from '../store/index'
-//Importamos las acciones que están en el fichero authSlice.ts
-import { authActions } from '../store/authSlice';
-
-// Importamos el useDispatch del react-redux
-import { useDispatch} from 'react-redux'
 
 import Menu from '../components/Menu'
 
 function Home() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-
   // Almacenamos en la variable userData lo que obtenemos del store usando el hook useSelector
   const userData = useSelector((state: RootState) => state.authenticator)
   // Comprobamos por la consola qué obtenemos del store
   console.log(userData)
 
-  const handleSubmit = (e: any) => {
-    // Para que no mande el formulario, sino que haga lo que yo le diga.
-    e.preventDefault();
-
-    dispatch(authActions.logout())
-    navigate('/')
-  };
-
   return (
     <>
-      <Typography variant="h2" color="primary.main">
-          Página Home de Moisés Antonio Pestano Castro
-
-          <br />
-          <br />
-
-          Logueado como usuario {userData.userName}, con rol de {userData.userRol}
-      </Typography>
-
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-        <Button
-          type="submit"
-          variant="contained"
-          color="secondary"
-          size="large"
-          onClick={handleSubmit}
-        >
-          SALIR
-        </Button>
-      </div>
-
       <Menu/>
     </>
   );
