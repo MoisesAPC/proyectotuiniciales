a65dc1a7f54e40958dd0edb759c7fea4770aa7ca
MoisesAPC <moipc647@gmail.com>
2024-11-12 12:18:40 +0000

    Avance formulario

--- frontend/src/components/Dahsboard.tsx
+++ frontend/src/components/Dahsboard.tsx
@@ -1,9 +1,77 @@
 import { Box } from '@mui/material';
 import { useState } from 'react';
-import Grid from '@mui/material/Grid2'
-import TextField from '@mui/material/TextField'
+import Grid from '@mui/material/Grid2';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Divider from '@mui/material/Divider';
 
 function Dashboard() {
+
+  // Datos del formulario {Nombre, Marca, Tipo}
+  const [data, setData] = useState({nombre:'', marca:'', tipo:'', precio:'0.00'})
+  const [formValido, setFormValido] = useState(false);  // Para comprobar que todos los campos required hayan sido rellenados previamente
+
+  // Si todos los campos con "required" han sido rellenados
+  // "esValido" pasará a ser a true, activando el botón de Enviar
+  const validarFormulario = (nombre: string, marca: string, tipo: string) => {
+    const esValido =
+      nombre.trim() !== '' &&
+      marca.trim() !== '' &&
+      tipo.trim() !== '';
+    setFormValido(esValido);
+  };
+
+  const handleChangeNombre = (e: { target: { value: string; }; }) =>{
+    setData({
+      ...data,
+      nombre: e.target.value
+    });
+
+    validarFormulario(e.target.value, data.marca, data.tipo);
+  };
+
+  const handleChangeMarca = (e: { target: { value: string; }; }) =>{
+    setData({
+      ...data,
+      marca: e.target.value
+    });
+
+    validarFormulario(data.nombre, e.target.value, data.tipo);
+  };
+
+  const handleChangeTipo = (e: { target: { value: string; }; }) =>{
+    setData({
+      ...data,
+      tipo: e.target.value
+    });
+
+    validarFormulario(data.nombre, data.marca, e.target.value);
+  };
+
+  const handleChangePrecio = (e: { target: { value: any; }; }) =>{
+    setData({
+      ...data,
+      precio: e.target.value
+    });
+  };
+
+  const handleInsertarDatos = (e: { preventDefault: () => void; }) => {
+    //Para que no mande el formulario, sino que haga lo que yo le diga
+    e.preventDefault();
+
+    console.log({
+      ...data
+    });
+
+    limpiarContenidos();
+  };
+
+  // Limpia los contenidos de las textboxes, las encuestas, etc
+  const limpiarContenidos = () => {
+    setData({nombre: '', marca: '', tipo: '', precio: '0.00'});
+    setFormValido(false);
+  };
+
 
     // Creamos el tipo itemtype. Este tipo será un objeto con un id opcional de tipo number
     // nombre, marca y tipo de tipo string y el precio de tipo number
@@ -38,6 +106,8 @@ function Dashboard() {
                 label='Nombre'
                 variant='outlined'
                 fullWidth
+                value={data.nombre}
+                onChange={handleChangeNombre}
               />
             </Grid>
 
@@ -48,6 +118,8 @@ function Dashboard() {
                 label='Marca'
                 variant='outlined'
                 fullWidth
+                value={data.marca}
+                onChange={handleChangeMarca}
               />
             </Grid>
 
@@ -58,6 +130,8 @@ function Dashboard() {
                 label='Tipo'
                 variant='outlined'
                 fullWidth
+                value={data.tipo}
+                onChange={handleChangeTipo}
               />
             </Grid>
 
@@ -69,12 +143,26 @@ function Dashboard() {
                 label='Precio'
                 variant='outlined'
                 fullWidth
+                value={data.precio}
+                inputProps={{ min: 0 }} // Así hacemos que el campo del precio no acepte números negativos
+                onChange={handleChangePrecio}
               />
+            </Grid>
+
+            {/* Botón de insertar datos */}
+            <Grid size={{ xs: 12, md: 10, lg: 5, xl: 6 }}>
+              <Button variant='contained' fullWidth onClick={handleInsertarDatos} disabled={!formValido}>
+                + INSERTAR DATOS
+              </Button>
+            </Grid>
+
+            {/* Línea divisora */}
+            <Grid size={12}>
+              <Divider />
             </Grid>
           </Grid>
         </Box>
     );
-
 }
 
 export default Dashboard;
